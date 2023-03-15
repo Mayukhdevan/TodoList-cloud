d5b3afbe1a4e5f8abdd14c003abc9800ac844a2d
Mayukhdevan <mayukhdevansahu@yahoo.com>
2023-03-15 11:12:23 +0000

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
-const App = () => <h1>App</h1>;
+const App = () => (
+  <Routes>
+    <Route exact path="/login" element={<Login />} />
+    <Route exact path="/" element={<Home />} />
+  </Routes>
+);
 
 export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,5 @@
+import "./index.css";
+
+const Login = () => <h1>Login</h1>;
+
+export default Login;
